2d170e273493fd09bbbf503d5f15301e8b3ae000
Maltonn <ymatsubayashi09@outlook.jp>
2021-09-15 14:57:56 +0900

    rewrite server

--- server/app.js
+++ server/app.js
@@ -1,95 +1,12 @@
-﻿const express = require('express');
-const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const port=3000;
-
 const io = require("socket.io")(server, {//cors許可
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
-
-/*
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-*/
-
-app.use(express.static('public'));
-
-app.use(function(req, res, next) { //CORSの許可 ローカルファイルからでもアクセスできるようにする
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-/* room_idの重複を防ぐためのやつ：適当に作っても普通は重複しなさそうなのでコメントアウト
-room_id_lst=[]
-app.get('/get-room-id',dfs=function(req,res){
-  params={}
-  req.url.split('?')[1].split('&').forEach((e)=>{params[e.split('=')[0]]=e.split('=')[1]})
-  charas= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1','2','3','4','5','6','7','8','9'];
-  id=""
-  num=params['n']
-  for(i=0;i<(num && 4<=num && num<=30?num:6);i++){
-    id+=charas[Math.floor(Math.random()*charas.length)]
-  }
-  if(room_id_lst.includes(id)){
-    return dfs(req,res)
-  }else{
-    room_id_lst.push(id)
-    res.json({'id':id})
-  }
-})
-*/
-
-/*サーバーにslideファイルを置いとく時用?
-const crypto = require('crypto');
-app.get('/admin', function(req, res){
-  res.sendFile(__dirname + '/html/admin.html');
-});
-app.get('/display',function(req,res){
-  //res.sendFile(__dirname + '/html/display/'+filename);  //ロック解除
-  res.redirect('/admin') //パスワードロックをかけたいとき
-});
-
-app.get('/', function(req, res){
-  //res.sendFile(__dirname + '/html/confirm.html');//特定のホームページに誘導
-  res.redirect('/client')
-});
-
-room_id_lst=[]
-
-
-
-filename='slide.html'
-app.post('/display',function(req,res){
-  if (req.body.username!="moscwa"){
-    res.redirect('/admin?wu');
-    return;
-  }
-  let shasum = crypto.createHash('sha1'); 
-  shasum.update(req.body.password+"saltvhiclk1ducdv8j2qmvzj3m4"); //一応saltを加える
-  let hash = shasum.digest('hex');
-  if (hash!="c85c9ef7b334a3a38eed9f7b8bbbd6967e236447"){
-    res.redirect('/admin?wp');
-    return;
-  }
-  res.sendFile(__dirname + '/html/display/'+filename);  
-})
-*/
-
-/*サーバーに投票とかのファイルを置いとくよう
-app.get('/comments', function(req, res){
-  res.sendFile(__dirname + '/html/archive.html');
-});
-app.get('/client', function(req, res){
-  res.sendFile(__dirname + '/html/client.html');
-});
-*/
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+  
 
 current_chc={}
 io.of('c').on('connection', (socket)=>{ //client
@@ -140,4 +57,5 @@ io.of('/a').on('connection',function(socket){
 server.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
 
